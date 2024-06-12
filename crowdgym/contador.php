c5d6eeb8edb308b8231f9725ff990d79059b585e
LZanata <118760279+LZanata@users.noreply.github.com>
2024-06-11 21:08:47 -0300

    Add files via upload

---
+++ crowdgym/contador.php
@@ -0,0 +1,28 @@
+<?php
+class Contador {
+    private $aluno;
+    
+    public function quantidadeAluno() {
+        echo '<div class="content"><h2>Quantidade de Alunos</h2><b>'. $this->getAluno().'</b></div>';      
+    }
+    
+    public function contarEntrada() {
+        $this->setAluno($this->getAluno()+1);
+    }
+    
+    public function contarSaida() {
+        $this->setAluno($this->getAluno()-1);
+    }
+    
+    public function __construct($al) {
+        $this->aluno = $al;
+    }   
+    
+    public function getAluno() {
+        return $this->aluno;
+    }
+    public function setAluno($aluno): void {
+        $this->aluno = $aluno;
+    }
+
+}
